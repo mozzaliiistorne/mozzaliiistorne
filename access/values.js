2c06cf5a5fcae7aa6f656effda942efff863ba61
mozzaliiistorne <mozzaliiistorne@protonmail.com>
2025-05-26 09:09:49 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -3,22 +3,22 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "01.01.2001"; // Дата рождения
-var fullname = "Фамилия Имя Отчество" // ФИО
-var name = "Имя" // Имя
+var birthdate = "11.05.2003"; // Дата рождения
+var fullname = "Кудінов Анатолій Іванович" // ФИО
+var name = "Анатолій" // Имя
 
 // Паспорт
-var passport_id = "123456789" // Номер паспорта
+var passport_id = "001821943" // Номер паспорта
 
 // КПП
-var kpp_id = "0123456789" // ИНН
+var kpp_id = "4983174853" // ИНН
 
 // Права
-var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_valid_until = "11.02.2027" // Права "Дійсні до"
 var rights_categories = "A, B" // Права "Категорії"
 var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_valid_until = "05.12.2024" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
